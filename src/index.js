d4481a88866f4266ce4a0aab49689527ae9317ec
Roshane Miller <millerroshane44@gmail.com>
2022-01-25 21:02:54 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const burgerMenu = document.getElementById("menu-icon");
+const hiddenNav = document.querySelector(".hidden-nav");
+const xButton = document.getElementById("xButton");
+const regDonBtns = document.querySelectorAll(".btn");
+const accordionBtns = document.querySelectorAll(".accordion");
+const accordionCont = document.querySelector(".hidden-nav-accordion");
+
+accordionCont.addEventListener("click", (e) => {
+  let target = e.target.closest(".accordion");
+  if (!target) return;
+  if (!accordionCont.contains(target)) return;
+  target.classList.toggle("active");
+});
+
+// for (btn of accordionBtns) {
+//   btn.addEventListener("click", openContent);
+// }
+
+for (btn of regDonBtns) {
+  btn.addEventListener("click", closeNav);
+}
+
+burgerMenu.addEventListener("click", showNav);
+xButton.addEventListener("click", closeNav);
+
+function showNav() {
+  hiddenNav.style.display = "grid";
+}
+function closeNav() {
+  hiddenNav.style.display = "none";
+}
+function closeContent() {
+  for (btn of accordionBtns) {
+    btn.classList.remove("active");
+  }
+}
